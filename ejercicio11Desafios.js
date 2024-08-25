53cc7f2a95cc37fbc4f9c6d5748b01c8ba604908
bobadillaFacundo <57062710+bobadillaFacundo@users.noreply.github.com>
2024-08-25 16:05:26 -0300

    dasafio11

--- ejercicio11Desafios.js
+++ ejercicio11Desafios.js
@@ -7,16 +7,38 @@ console.log(getIndexsForPalindrome('caababa')) // null
 
 function getIndexsForPalindrome(word) {
     const s = word.split('')
-    let r = []
+    const t = s.length / 2
+    let p = s.length - 1
+    let r = ''
+    let c = 0
+    let f = 0
+    let b = true
 
-    for (let index = 0; index < s.length/2; index++) {
-        for (let index2 = s.length/2; index2 < s.length; index2--) {
-            if (s[index] !== s[index2]) {
-                r=null
-                break
+    for (let index = 0; index < t; index++) {
+
+        if (s[index] !== s[p]) {
+            for (let index2 = 0; index2 < t + 1; index2++) {
+                f = s[index2]
+                if ((f === s[p])) {
+                    if (r === '')
+                        r = [index, index2]
+                    s[index] = f
+                    c++
+                }
             }
         }
+        p--
     }
+    if (c > 2) { return null }
+    if (r === '') { return [] } else return r
+}
 
-    return r
-  }
+
+// Ejemplos donde tu código falla
+console.log(getIndexsForPalindrome('racecar')); // Debería ser []
+console.log(getIndexsForPalindrome('abcdba')); // Debería ser [2,3]
+console.log(getIndexsForPalindrome('abcda')); // Debería ser [1,3]
+console.log(getIndexsForPalindrome('abccba')); // Debería ser []
+console.log(getIndexsForPalindrome('abccxaa')); // null
+console.log(getIndexsForPalindrome('abcba')); // []
+console.log(getIndexsForPalindrome('aacdaa')); // [2, 3] 
