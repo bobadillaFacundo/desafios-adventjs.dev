1e0db721f7efe4c3f5f7706a52f5de63d3ecd12a
bobadillaFacundo <57062710+bobadillaFacundo@users.noreply.github.com>
2025-04-19 17:07:59 -0300

    194-4-2025

--- ejericico7Desafios.js
+++ ejericico7Desafios.js
@@ -2,24 +2,38 @@ function drawGift(size, symbol) {
 
   let result = []
   let result2 = []
-    
-  for (let x = 0; x < size; x++) {
-   for (let y = 0; y < size*2; y++) {
-     if (y<size-x) result.push('-')
-     if (y>size-x) result.push('#')
-     if (y===2*size-1) result.push('#\n')
-  } 
-  }
-    result.pop()
-    for (let x = 0; x < size; x++) {
+
+  for (let x = 1; x < size; x++) {
     for (let y = 0; y < size * 2; y++) {
-if (y<x) result.push('-')
-     if (y>x) result.push('#') 
-         if (x===y) result.push('\n')
-    
+      if (y < size - x) result.push(['-'])
+      if (y > size - x) result.push(['#'])
+      if (y === (2 * size) - 1) result.push(['\n'])
     }
   }
-  return result.join('')
+  result.pop()
+
+  // for (let x = 0; x < size; x++) {
+  //   for (let y = 0; y < size * 2; y++) {
+  //     if (x === y) result[x].push('\n')
+  //     if (y < x) result[x].push('-')
+  //     if (y > x) {
+  //       result[x].push('#')
+  //     }
+
+  //   }
+  // }
+
+  // for (let x = 1; x < (size * 2) - 1; x++) {
+  //   for (let y = 0; y < (size * 2) - 1; y++) {
+  //     if (x < (((size * 2) - 1) * (size - 2))) {
+  //       for (let i = 0; i < size - 2; i++) {
+  //         result[(((size * 2) - 1) * (size - 2)) - x] = symbol
+  //       }
+  //     }
+  //   }
+  // }
+
+  return result
 }
 
-console.log(drawGift(4,'+'))
+console.log(drawGift(5, '+'))
--- ejericico9Desafios.js
+++ ejericico9Desafios.js
@@ -14,18 +14,16 @@ console.log(adjustLights(['🔴', '🔴', '🔴']))
 // -> 1 (cambias la segunda luz a 🟢)
 
 function adjustLights(lights) {
-let c=0
-const arr =[]
+    let c = 0
+    const arr = []
 
     for (let name of lights) {
         arr.push(name)
     }
-for (let index = 0; index < lights.length; index++) {
-    if(lights[index+1]===undefined) break
- if ((lights[index]==='🟢')&&(lights[index+1]==='🟢'&&(lights[index-1]!=='🟢'))) 
- {c++} else 
-if ((lights[index]==='🔴')&&(lights[index+1]==='🔴')&&(lights[index-1]!=='🔴'))
-{c++}   
-     }
-return c
+    for (let index = 0; index < lights.length; index++) {
+        if (lights[index + 1] === undefined) break
+        if ((lights[index] === '🟢') && (lights[index + 1] === '🟢' && (lights[index - 1] !== '🟢'))) { c++ } else
+            if ((lights[index] === '🔴') && (lights[index + 1] === '🔴') && (lights[index - 1] !== '🔴')) { c++ }
+    }
+    return c
 }
